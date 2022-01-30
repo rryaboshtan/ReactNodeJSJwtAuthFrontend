ef600121cc20b3297e1e98870863454773f600af
rryaboshtan <rrvworkroman@gmail.com>
2022-01-30 03:45:51 +0200

    add useContext

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,14 +1,16 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useContext, useState } from 'react';
+import { Context } from '../index';
 
 const LoginForm: FC = () => {
    const [email, setEmail] = useState<string>('');
    const [password, setPassword] = useState<string>('');
+   const { store } = useContext(Context);
    return (
       <div>
          <input onChange={e => setEmail(e.target.value)} value={email} type='text' placeholder='Email'></input>
          <input onChange={e => setPassword(e.target.value)} value={password} type='password' placeholder='Password'></input>
-         <button>Login</button>
-         <button>Registration</button>
+         <button onClick={() => store.login(email, password)}>Login</button>
+         <button onClick={() => store.registration(email, password)}>Registration</button>
       </div>
    );
 };
